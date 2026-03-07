2cdfd43eb83b1cabe9e6a83063c32da61a9ae24d
Anupam Biswas <ansi2u@gmail.com>
2026-03-07 13:40:08 +0400

    GTM added

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,6 +48,16 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WMZSRR3M');`,
+          }}
+        />
         {/* Prevent flash of unstyled content: apply dark class before first paint */}
         <script
           dangerouslySetInnerHTML={{
@@ -56,6 +66,15 @@ export default function RootLayout({
         />
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WMZSRR3M"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Providers>
           {children}
           <Toaster />
